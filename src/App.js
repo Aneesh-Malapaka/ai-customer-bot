c0dda4834e6476bfe4ad8f3d8ed98556cf5a7a47
Aneesh Malapaka <aneeshmalapaka@gmail.com>
2024-08-13 17:43:19 +0530

    added backend deployed url for fetch

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     setIsLoading(true);
     try {
       // call Gemini Api to get a response
-      const response = await fetch("http://localhost:5000/", {
+      const response = await fetch("https://ai-customer-bot-server.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
